9c3a9ccf6d64ab29ab6b254662f7986613e0c076
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-18 21:52:37 +0300

    add class JSONFileManager

--- src/file_manager.py
+++ src/file_manager.py
@@ -1,1 +1,16 @@
 from abstract_classes import FileManager
+
+
+class JSONFileManager(FileManager):
+    """
+    Класс для работы с json-файлом
+    """
+
+    def write(self, data):
+        pass
+
+    def read(self):
+        pass
+
+    def delete(self):
+        pass
