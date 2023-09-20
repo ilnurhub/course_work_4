44dcf2ca2f5698ab435ebd55b86ed0980df1b2e2
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-20 20:39:54 +0300

    correct get_vacancies

--- src/site_api.py
+++ src/site_api.py
@@ -12,7 +12,16 @@ class HeadHunterAPI(APIManager):
         self.formatted_vacancies = []
 
     def get_vacancies(self, keyword: str) -> list:
-        pass
+        params = {
+            'text': f'name:{keyword}',  # Текст фильтра. В имени должно быть  ключевое слово
+            'area': 1,  # Поиск ощуществляется по вакансиям города Москва
+            'per_page': 100  # Кол-во вакансий на 1 странице
+        }
+        responce = requests.get('https://api.hh.ru/vacancies', params=params)
+        vacancies = responce.json()
+        self.vacancies_list = vacancies['items']
+        result = self.format_data()
+        return result
 
     def format_data(self):
         pass
