3ea4f844606c941c15e73ed7d2b82f0751b3ca37
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-20 20:39:05 +0300

    add def __init__()

--- src/site_api.py
+++ src/site_api.py
@@ -1,4 +1,5 @@
 from abstract_classes import APIManager
+import requests
 
 
 class HeadHunterAPI(APIManager):
@@ -6,7 +7,11 @@ class HeadHunterAPI(APIManager):
     Класс для работы с сайтом https://hh.ru/
     """
 
-    def get_vacancies(self, keyword: str) -> dict:
+    def __init__(self):
+        self.vacancies_list = None
+        self.formatted_vacancies = []
+
+    def get_vacancies(self, keyword: str) -> list:
         pass
 
     def format_data(self):
