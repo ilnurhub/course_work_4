8070ffd0a1e6bc05999db72d20ff3a68d1ffcec9
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-21 18:33:15 +0300

    add def calculate_average_salary()

--- src/vacancy.py
+++ src/vacancy.py
@@ -2,5 +2,12 @@ class Vacancy:
     """
     Класс для работы с вакансиями
     """
-    def __init__(self, vacancy_data:dict):
+
+    def __init__(self, vacancy_data: dict):
+        pass
+
+    def calculate_average_salary(self, salary_min: int, salary_max: int) -> int:
+        """
+        Вычисляет среднюю зарплату
+        """
         pass
