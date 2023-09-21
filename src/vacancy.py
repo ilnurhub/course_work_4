45a5c150fe2896d0e4f542f612478e28fb925309
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-21 21:38:01 +0300

    fill def validate_data()

--- src/vacancy.py
+++ src/vacancy.py
@@ -10,7 +10,17 @@ class Vacancy:
         """
         Валидирует данные вакансии
         """
-        pass
+        if not isinstance(vacancy_data['name'], str):
+            vacancy_data['name'] = '-'
+        if not isinstance(vacancy_data['url'], str):
+            vacancy_data['url'] = '-'
+        if not isinstance(vacancy_data['salary_min'], int):
+            vacancy_data['salary_min'] = 0
+        if not isinstance(vacancy_data['salary_max'], int):
+            vacancy_data['salary_max'] = 0
+        if not isinstance(vacancy_data['description/requirement'], str):
+            vacancy_data['description/requirement'] = '-'
+        return vacancy_data
 
     def calculate_average_salary(self, salary_min: int, salary_max: int) -> int:
         """
