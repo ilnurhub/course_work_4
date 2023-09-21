2d2cc0d944a45afa70070c4f9995b8e3edf6d428
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-21 18:35:03 +0300

    add def validate_data()

--- src/vacancy.py
+++ src/vacancy.py
@@ -6,6 +6,12 @@ class Vacancy:
     def __init__(self, vacancy_data: dict):
         pass
 
+    def validate_data(self, vacancy_data: dict):
+        """
+        Валидирует данные вакансии
+        """
+        pass
+
     def calculate_average_salary(self, salary_min: int, salary_max: int) -> int:
         """
         Вычисляет среднюю зарплату
