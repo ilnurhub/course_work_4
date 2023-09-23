4bdc31a98c4a01b58dab4dea5b01c4726449ecdd
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-23 20:31:35 +0300

    add def __init__()

--- src/file_manager.py
+++ src/file_manager.py
@@ -1,10 +1,16 @@
 from abstract_classes import FileManager
+from config import ROOT_PATH, FILENAME
+import os
 
 
 class JSONFileManager(FileManager):
     """
     Класс для работы с json-файлом
     """
+
+    def __init__(self):
+        self.filename = FILENAME + '.json'
+        self.filepath = os.path.join(ROOT_PATH, 'data', self.filename)
 
     def write(self, data):
         pass
